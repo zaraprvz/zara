2109c9f57f4c9a537df48c0e4ae2d55773b80b1d
zaraprvz <127175048+zaraprvz@users.noreply.github.com>
2023-09-22 20:31:18 -0500

    Add files via upload

---
+++ treasure island game.py
@@ -0,0 +1,15 @@
+print("Welcome treasure island")
+print("your mission is to hunt gold or diamoind ")
+ 
+choice1 = input("your on a boat where would you like to stop ? hotel or camping areas ?\n")
+if choice2 == hotel:
+  input("which hotel room would you like to pick ? the one with yellow , red or blue door?\n")
+if choice3 == "yellow":
+  print("this room has a mystery box , its full of gold ,you've won!\n")
+elif choice3 == "red":
+   print("Your found the dimond treasure box! yay! you won the game !")
+elif choice3 == "blue":
+   print("you got attacked by beast , game over !")
+else:
+    print("The monster attacked you, game over!")
+    
